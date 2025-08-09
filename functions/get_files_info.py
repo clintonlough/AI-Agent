36128d62562dab558a903d672182c0bea827479c
clintonlough <clinton.lough@gmail.com>
2025-08-09 17:39:02 +1000

    created a calc tool and a get_file_info tool that provides directory info

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    abs_path = os.path.abspath(full_path)
+    abs_working_path = os.path.abspath(working_directory)
+
+    if abs_path == abs_working_path or abs_path.startswith(abs_working_path + os.sep):
+        if not os.path.isdir(abs_path):
+            return f'Error: "{directory}" is not a directory'
+        else:
+            contents =  os.listdir(abs_path)
+            directory_contents = ""
+            for content in contents:
+                name = str(content)
+                temp_path = os.path.join(full_path, content)
+                file_size = os.path.getsize(temp_path)
+                is_dir = not os.path.isfile(temp_path)
+                item_info = f"- {name}: file_size={file_size} bytes, is_dir={is_dir}"
+                directory_contents += item_info + "\n"
+            return directory_contents[:-1]
+
+    else:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+def main():
+    pass
+
+if __name__ == "__main__":
+    # Your manual tests here
+    print(get_files_info("calculator", "."))
--- main.py
+++ main.py
@@ -2,22 +2,55 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-def main():
-    print("Hello from ai-agent! \n")
-    if len(sys.argv) == 2:
+def get_user_prompt():
+    #Checks for a prompt from the user as an argv
+    if len(sys.argv) >= 2:
         user_prompt = sys.argv[1]
-        response = client.models.generate_content(model = "gemini-2.0-flash-001", contents=user_prompt )
-        print (response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]    #Exits program if user does not provide a prompt
     else:
         print("Prompt not provided. Exiting program...")
         sys.exit(1)
+    return messages
+
+
+#Gets a response from the Gemini LLM using the user prompt.
+def get_response(user_prompt,verbose):
+    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents=user_prompt )
+
+    #Prints results based on flags
+    if verbose == True:
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print (response.text)
+
+    return response
+
+def check_verbose():
+    for arg in sys.argv[1:]:
+        if arg == "--verbose":
+            return True
+    return False
+
+
+def main():
+
+    messages = get_user_prompt()
+    verbose = check_verbose()
+    response = get_response(messages,verbose)
+
+
+        
+
+
 
 if __name__ == "__main__":
     main()
