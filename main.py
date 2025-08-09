cf95f3dc209d18fa60e1c3156be63e3f154a38d6
clintonlough <clinton.lough@gmail.com>
2025-08-09 14:35:53 +1000

    generates a response using gemini API from user prompt and displays result

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def main():
+    print("Hello from ai-agent! \n")
+    if len(sys.argv) == 2:
+        user_prompt = sys.argv[1]
+        response = client.models.generate_content(model = "gemini-2.0-flash-001", contents=user_prompt )
+        print (response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("Prompt not provided. Exiting program...")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
